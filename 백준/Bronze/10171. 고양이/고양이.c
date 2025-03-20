34f0e8f2800dd7b492ce51b1ba3b35e0326070d7
Yeho Na <nayeho@gmail.com>
2025-03-20 11:54:41 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%s\n", "\\    \/\\");
+    printf("%s\n", " \)  \( \'\)");
+    printf("%s\n", "\(  \/  \)");
+    printf("%s\n", " \\\(\_\_\)\|");
+    
+    return 0;
+}
