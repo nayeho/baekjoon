f7511928bf5d317a660f0314adf47d843ebeb296
Yeho Na <nayeho@gmail.com>
2025-03-21 17:58:58 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.c
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -6,10 +6,10 @@ int main(void)
     scanf("%d", &n);
     
     int arr[n];
-    for(int i = 0; i < n; i++) scanf(" %d", &arr[i]);
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
     
     int v;
-    scanf(" %d", &v);
+    scanf("%d", &v);
     
     int cnt = 0;
     for(int i = 0; i < n; i++) if (arr[i] == v) cnt++;
