5a5694a9ceb0c2434ebb3575cbe3f75c850e654c
Yeho Na <nayeho@gmail.com>
2025-03-21 13:28:20 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            printf("%s", "*");    
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
