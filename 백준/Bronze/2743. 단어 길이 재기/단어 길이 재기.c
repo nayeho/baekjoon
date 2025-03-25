e842de9e2a00e74444f65f56d98512a73bd9fc36
Yeho Na <nayeho@gmail.com>
2025-03-25 11:29:36 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -4,12 +4,11 @@ int main(void)
 {
     char s[101];
     scanf("%s", s);
-    
+
     int length = 0;
-    char* p = s;
-    while(*p++) length++;
-    
-    printf("%d", length);
-    
+    while (s[length] != '\0') length++;
+
+    printf("%d\n", length);
+
     return 0;
 }
