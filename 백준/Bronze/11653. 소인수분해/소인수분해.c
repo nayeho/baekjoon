0a5cc8ef7807efb9591da0a6e32203f27591a545
Yeho Na <nayeho@gmail.com>
2025-03-27 11:50:05 +0900

    [Bronze I] Title: 소인수분해, Time: 24 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    
+    while(N > 1)
+    {
+        for(int i = 2; i <= N; i++)
+        {
+            if(N % i == 0)
+            {
+                printf("%d\n", i);
+                N = N / i;
+                break;
+            }
+        }
+    }
+    return 0;
+}
