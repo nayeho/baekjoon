064621e8907470d3f2da85637d58280df2b806b2
Yeho Na <nayeho@gmail.com>
2025-04-16 16:42:00 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.c
+++ 백준/Bronze/2566. 최댓값/최댓값.c
@@ -2,25 +2,32 @@
 
 int main(void)
 {
-    int arr[9][9];
-    for(int i = 0; i < 9; i++) for(int j = 0; j < 9; j++) scanf("%d", &arr[i][j]);
-    
-    int max = arr[0][0];
-    int max_row_index = 1;
-    int max_column_index = 1;
-    
-    for(int i = 0; i < 9; i++)
-    {
-        for(int j = 0; j < 9; j++)
-        {
-            if(arr[i][j] > max)
-            {
-                max = arr[i][j];
-                max_row_index = i + 1;
-                max_column_index = j + 1;
-            }
-        }
-    }
-    printf("%d\n%d %d", max, max_row_index, max_column_index);
-    return 0;
+	// 입력부
+	int array[9][9];
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			scanf("%d", &array[i][j]);
+		}
+	}
+	// 논리부 
+	int max = -1;
+	int max_row = 0;
+	int max_col = 0;
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (array[i][j] > max)
+			{
+				max = array[i][j];
+				max_row = i;
+				max_col = j;
+			}
+		}
+	}
+	printf("%d\n%d %d\n", max, max_row + 1, max_col + 1);
+	return 0;
 }
